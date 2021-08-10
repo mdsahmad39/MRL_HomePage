8623404e5e0c2012fcaaff1a499d7f50256e9061
Sarfaraz Ahmad Mahammad <45324530+mdsahmad39@users.noreply.github.com>
2021-08-10 10:21:39 +0530

    changing layout

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-$(window).on("scroll", function() {
-  if($(window).scrollTop()) {
-    $('nav').addClass('black');
+var divBlocks = ['div1', 'div2', 'div3'], n = 0, limit = 3;
+
+var interval = setInterval(blockDisplay, 2000);
+
+function blockDisplay() {
+  if (n > 2) {
+    for (var i = 0; i < divBlocks.length; i++) {
+      document.getElementById(divBlocks[i]).style.display = 'none';
+    }
+    document.getElementById(divBlocks[1]).style.display = '';
+    clearInterval(interval);
   } else {
-    $('nav').removeClass('black');
-  };
-});
+    for (var i = 0; i < divBlocks.length; i++) {
+      document.getElementById(divBlocks[i]).style.display = 'none';
+    }
+    document.getElementById(divBlocks[n]).style.display = '';
+    console.log(1);
+    n += 1;
+  }
+}
