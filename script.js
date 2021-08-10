c95b0d08924bbb6f1088f580d31bbabef6991617
Sarfaraz Ahmad Mahammad <45324530+mdsahmad39@users.noreply.github.com>
2021-08-10 14:36:20 +0530

    final changes

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-var divBlocks = ['div1', 'div2', 'div3'], n = 0, limit = 3;
+var divBlocks = ['div1', 'div2', 'div3', 'div4'], n = 0, limit = 3;
 
-var interval = setInterval(blockDisplay, 15000);
+var interval = setInterval(blockDisplay, 10000);
 
 function blockDisplay() {
   if (n > 2) {
     for (var i = 0; i < divBlocks.length; i++) {
       document.getElementById(divBlocks[i]).style.display = 'none';
     }
-    document.getElementById(divBlocks[1]).style.display = '';
+    document.getElementById(divBlocks[3]).style.display = '';
     clearInterval(interval);
   } else {
     for (var i = 0; i < divBlocks.length; i++) {
       document.getElementById(divBlocks[i]).style.display = 'none';
     }
     document.getElementById(divBlocks[n]).style.display = '';
-    console.log(1);
     n += 1;
   }
 }
